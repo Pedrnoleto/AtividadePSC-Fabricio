dcaaecdc2efa27051f67e352adfaba500b6db022
Pedrnoleto <149097151+Pedrnoleto@users.noreply.github.com>
2024-04-04 19:03:54 -0300

    Questao15.java

---
+++ Lista1/Questao15.java
@@ -0,0 +1,28 @@
+// Construa um programa que calcule as raízes de uma equação de 2º grau (Ax2+Bx+C). Sendo que os valores A,B e C são fornecidos pelo usuário. Considere que tem duas raízes.
+
+
+
+import java.util.Scanner;
+
+public class Main {
+ public static void main(String[] args){
+
+  Scanner input = new Scanner(System.in);
+
+  System.out.println("Digite o valor de A: ");
+   double a = input.nextDouble();
+
+  System.out.println("Digite o valor de B: ");
+   double b = input.nextDouble();
+
+  System.out.println("Digite o valor de C: ");
+   double c = input.nextDouble();
+
+      
+     
+   
+
+   
+ }
+  
+}
